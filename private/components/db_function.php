45005447ef86fd92e0656d0238b927e7438d6303
QuanT <88959861+Quan987@users.noreply.github.com>
2024-07-17 01:28:22 -0400

    add register_user & check_register_duplicate function

--- private/components/db_function.php
+++ private/components/db_function.php
@@ -1,5 +1,4 @@
-<?php 
-    require_once (PRIVATE_PATH . '/path.php');
+<?php require_once (PRIVATE_PATH . '/path.php');
 
     function connect_db() {
         $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
@@ -7,16 +6,41 @@
             echo "Connection failed: " . $conn->connect_error;
             exit;
         }
-        echo "Connected successfully";
-        
-        
-        
+        echo "Connected successfully";   
+        return $conn;
     }
 
     function db_disconnect($connection) {
         if(isset($connection)) {
             $connection -> close();
         }
+    }
+
+    function register_user($info) {
+        global $db;
+        // $hashed_password = password_hash($info["password"], PASSWORD_DEFAULT);
+        $query = "INSERT INTO user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param('ssss', $info["first_name"], $info["last_name"], $info["email"], $info["password"]);
+    
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+
+    function check_register_duplicate($userEmail) {
+        global $db;
+        $query = "SELECT email FROM user ORDER BY id ASC";
+        $stmt = $db->query($query);
+        while($result = $stmt->fetch_assoc()) {
+            if ($userEmail === $result["email"]) {
+                return true;
+            }
+        }
+        return false;
+        
     }
 
 ?>
