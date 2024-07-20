b1aae5d5e1df245e2f29d1e4aa7c5af74b5eb475
QuanT <88959861+Quan987@users.noreply.github.com>
2024-07-20 18:32:16 -0400

    Add edit functionality

---
+++ public/pages/edit.php
@@ -0,0 +1,37 @@
+<?php 
+
+require_once('../../private/initialize.php');
+require_once('index.php');
+
+    if($_SERVER["REQUEST_METHOD"] !== "POST") {
+        header('Location: ' . 'index.php');
+        exit;
+    } else {
+        $user_info = [
+            "id" => $_SESSION["id"],
+            "passwordID" => $_POST["password-box-id"],
+            "passwordValue" => strip_space($_POST["password-box-value"])
+        ];
+        
+        if(update_password($user_info)) {
+            $_POST["password-box-id"] = "";
+            $_POST["password-box-value"] = "";
+            echo <<<EOF
+                <script type="text/javascript">
+                alert("Successful");
+                window.location.href="index.php";
+                </script>;
+            EOF;
+        } else {
+            echo <<<EOF
+                <script type="text/javascript">
+                alert("Password Cannot Be Update");
+                window.location.href="index.php";
+                </script>;
+            EOF;
+        }
+       
+
+    }
+
+?>
