6d577e6d59c262dc975c33079d07fc48fc9b7609
QuanT <88959861+Quan987@users.noreply.github.com>
2024-07-20 18:34:34 -0400

    Add add encode_value & strip_space function

--- private/components/function.php
+++ private/components/function.php
@@ -3,4 +3,21 @@
         return htmlspecialchars(urlencode($url));
     }
 
+    function duplicate_password_message() {
+        echo <<<EOF
+            <script type="text/javascript">
+            alert("Password already exist, please generate a different one");
+            window.location.href="index.php";
+            </script>;
+        EOF;
+    }
+
+    function encode_value($value) {
+        return htmlspecialchars($value);
+    }
+
+    function strip_space($value) {
+        return preg_replace('/\s+/', '', $value);
+    }
+
 ?>
